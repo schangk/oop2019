f589af9d5d61eecdb6dc59f9db016df1b9cbd053
Schængk <30302820+schangk@users.noreply.github.com>
2019-02-28 11:02:21 +0200

    Add files via upload

---
+++ 28-2-2.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+struct Student
+{
+    char name [100];
+    long long egn;
+    long fn;
+    long marks [10];
+};
+
+void initialize(Student& s)
+{
+    char neim [100];
+    cin >> neim;
+    strcpy(s.name,neim);
+    long long data;
+    cin >> data;
+    s.egn = data;
+    cin >> data;
+    s.fn = data;
+    for(int index = 0; index < 10; index++)
+    {
+        cin >> data;
+        s.marks[index] = data;
+    }
+};
+
+int main ()
+{
+    Student gosho;
+    initialize(gosho);
+
+
+    return 0;
+}
---
+++ 28-2.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+int main ()
+{
+    /*char input [100];
+    cin >> input;
+    ofstream fi ("file.txt");
+    fi << input;
+    fi.close();
+
+    char output [100];
+
+    ifstream fii ("file.txt");
+    fii.getline(output,100);
+    cout << output;
+    fii.close();*/
+
+    //cout << "[i]nput or [o]utput stream: ";
+    //char stream;
+    //cin >> stream;
+
+    cout << "File name: ";
+    char failneim [100];
+    cin >> failneim;
+
+    char fail [100] = "null";
+
+    //if(stream == 'i')
+    //{
+        int cnt = 0;
+        ifstream strim (failneim);
+        /*strim.getline(fail,100);
+        cout << fail;*/
+        while(fail != "\0")
+        {
+            strim.read(fail,100);
+            cout << fail;
+            cnt++;
+        }
+
+        strim.close();
+
+        cout << cnt;
+    //}
+    /*else if(stream == 'i')
+    {
+        ofstream strim (failneim);
+    }*/
+    /*else
+    {
+        cout << "Is this UI so hard for you ?";
+    }*/
+
+    return 0;
+}
